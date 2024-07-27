6f43265e26c7bb63ecf266f3a100be12773b8015
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-07-27 19:54:53 +0530

    git commit from sts

--- IDE/src/p1/TestIDE.java
+++ IDE/src/p1/TestIDE.java
@@ -2,7 +2,7 @@ package p1;
 public class TestIDE {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to STS IDE,hii,"+args[0]);
+		System.out.println("Welcome to STS IDE and connected to git repository,hii,"+args[0]);
 	}
 
 }
