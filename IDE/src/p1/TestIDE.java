fee7e0a235381bf0875b0c2714cd8b9bc6f82386
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-07-27 20:39:12 +0530

    git push from STS

--- IDE/src/p1/TestIDE.java
+++ IDE/src/p1/TestIDE.java
@@ -2,7 +2,7 @@ package p1;
 public class TestIDE {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to STS IDE and pushing to git from sts,hii,"+args[0]);
+		System.out.println("Welcome to STS IDE push it to git from sts,hii,"+args[0]);
 	}
 
 }
