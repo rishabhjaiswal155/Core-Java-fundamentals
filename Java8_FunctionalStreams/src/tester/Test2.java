b0b0beb45d39a0941c99c31a18a0edff40487b4d
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-08-29 16:21:25 +0530

    Creating ArrayList<Integer>,populating it and converting it into ParallelStream(using parallelStream()),also display its contents.

--- Java8_FunctionalStreams/src/tester/Test2.java
+++ Java8_FunctionalStreams/src/tester/Test2.java
@@ -7,8 +7,15 @@ public class Test2 {
 	public static void main(String[] args) {
 		//Create ArrayList<Integer>,populate it and convert it into sequentialStream and display its contents.
 				ArrayList<Integer> list=new ArrayList<>(Arrays.asList(10,-40,-100,45,8,2,56,42));
+				System.out.println("Sequential stream");
 				list
 				.stream()
+				.forEach(System.out::println);
+				
+				//convert it into parallelStream and display its contents.
+				System.out.println("parallel stream");
+				list
+				.parallelStream()
 				.forEach(System.out::println);
 	}
 }
