da022ba48759cd1a722183e0bcddb29d3485c8f6
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-09-04 14:01:45 +0530

    JointAccount Scenario(Shared Resource),leads to Racing Condition and DirtyRead(due to critical section) in multi-threaded system.(Need of Synchronization).

---
+++ Synchronization_in_MultiThreading/src/com/app/core/JointAccount.java
@@ -0,0 +1,31 @@
+package com.app.core;
+
+public class JointAccount {
+	private double balance;
+
+	public JointAccount(double balance) {
+		super();
+		this.balance = balance;
+	}
+
+	@Override
+	public String toString() {
+		return "JointAccount [balance=" + balance + "]";
+	}
+	
+	public void updateBalance(double amount) throws InterruptedException {
+		System.out.println("updatation started by "+Thread.currentThread().getName());
+	    balance=balance+amount;
+	    System.out.println("Cancelling Update after a delay");
+	    Thread.sleep(17);
+	    balance=balance-amount;
+		System.out.println("updatation over by "+Thread.currentThread().getName());
+	}
+	
+	public double checkBalance() throws InterruptedException {
+		System.out.println("checkingBalance started by "+Thread.currentThread().getName()); 
+		Thread.sleep(37);
+		System.out.println("checkingBalance over by "+Thread.currentThread().getName());  
+		return balance;
+	}
+}
