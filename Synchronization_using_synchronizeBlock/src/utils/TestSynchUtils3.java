58cce16b8410fc090f8114444a911667b759a61a
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-09-04 18:56:38 +0530

    observing unSynchronized behavior in output when t1 calls test() which is synchronized and t2 calls testMeAgain() which is unsynchronized.

---
+++ Synchronization_using_synchronizeBlock/src/utils/TestSynchUtils3.java
@@ -0,0 +1,35 @@
+package utils;
+import java.io.IOException;
+public class TestSynchUtils3 {
+	private static boolean exit=false;
+	public static void main(String[] args) throws InterruptedException, IOException {
+		SynchronizeUtils utils=new SynchronizeUtils();
+		//creating threads using ctor Thread(Runnable instance,String name) using lambda expression
+		Thread t1=new Thread(()->{
+			while(!exit) {
+				utils.test();
+			}
+		},"t1");
+		
+		Thread t2=new Thread(()->{
+			while(!exit) {
+				utils.testMeAgain();
+			}
+		},"t2");
+		//here both t1 and t2 threads are in NEW state
+		
+		t1.start();//state:Runnable
+		t2.start();//state:Runnable
+		//main blocked on IO
+		System.out.println("press enter button to stop");
+		System.in.read();
+	    //main blocked on join
+		System.out.println("main waiting for children thread to complete execution");
+		System.out.println("stoping execution of children threads:exit");
+		exit=true;
+		//no orphans
+		t1.join();
+		t2.join();
+		System.out.println("main over");	
+	}
+}
