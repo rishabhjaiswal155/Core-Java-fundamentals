002b2a3bcc95a92bb96ea72950ca6e9fc7692243
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-09-04 18:54:44 +0530

    observing Synchronize behavior in output when t1 calls test() synchronized method and t2 calls testMe() synchronized method.

--- Synchronization_using_synchronizeBlock/src/utils/SynchronizeUtils.java
+++ Synchronization_using_synchronizeBlock/src/utils/SynchronizeUtils.java
@@ -14,4 +14,25 @@ public class SynchronizeUtils  {
 		}
 		System.out.println("Exitted test: "+Thread.currentThread().getName());
 	}
+	
+	public synchronized void testMe() {
+		System.out.println("Entered in testMe: "+Thread.currentThread().getName());
+		try {
+			Thread.sleep(r1.nextInt(15)+1);//Generating random number from 1-14
+		} catch (InterruptedException e) {
+			System.out.println(Thread.currentThread().getName()+" got exception "+e);
+		}
+		System.out.println("Exitted testMe: "+Thread.currentThread().getName());
+	}
+	
+
+	public  void testMeAgain() {
+		System.out.println("Entered in testMeAgain: "+Thread.currentThread().getName());
+		try {
+			Thread.sleep(r1.nextInt(15)+1);//Generating random number from 1-14
+		} catch (InterruptedException e) {
+			System.out.println(Thread.currentThread().getName()+" got exception "+e);
+		}
+		System.out.println("Exitted testMeAgain: "+Thread.currentThread().getName());
+	}
 }
